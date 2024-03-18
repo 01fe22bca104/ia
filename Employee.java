82f66ad6e490af96455e43fd5512f3ea9e15d9f8
saadhvi <01fe22bca104@kletech.ac.in>
2024-03-18 14:26:52 +0530

    Program to display employee information

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+   int id=104;
+   String name="Saadhvi";
+   double salary=700000;
+
+   public void display()
+   {
+    System.out.println("ID:"+id);
+    System.out.println("Name"+name);
+    System.out.println("Salary"+salary);
+   }
+    
+}
